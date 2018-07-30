d3f56521932d150b48e4a48f3dda2eab219ba5b5
Denys Dovhan <denysdovhan@gmail.com>
2018-07-30 16:11:03 +0300

    Upgrade to gulp@4

--- gulpfile.js
+++ gulpfile.js
@@ -35,24 +35,24 @@ gulp.task('render', () =>
     .pipe(gulp.dest('dist'))
 );
 
-gulp.task('build', ['styles', 'render']);
-
-gulp.task('watch', ['build', 'server'], () => {
-  gulp.watch(['styles/**/*'], ['styles']);
-  gulp.watch(['**/*.{jade,md}'], ['render']);
-});
+gulp.task('build', gulp.series('styles', 'render'));
 
 gulp.task('server', () => {
   express().use(express.static('dist')).listen(8000);
   console.log('Server is running on http://localhost:8000');
 });
 
-gulp.task('deploy', ['build'], () =>
+gulp.task('watch', gulp.series('build', 'server', () => {
+  gulp.watch(['styles/**/*'], ['styles']);
+  gulp.watch(['**/*.{jade,md}'], ['render']);
+}));
+
+gulp.task('deploy', gulp.series('build', () =>
   gulp.src('dist/**/*')
     .pipe(deploy({
       push: true,
       message: `Update ${moment(new Date()).format('lll')}`
     }))
-);
+));
 
-gulp.task('default', ['watch']);
+gulp.task('default', gulp.series('watch'));
